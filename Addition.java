a7e3ccee8df90ed4acb69710044ae31c26c86c9f
Aishwarya-Loni15 <aishwaryaloni872@gmail.com>
2026-09-05 18:31:51 +0530

    Daily Java Learning

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String [] data){
+        int a = 2 ; 
+        int b = 8  ; 
+
+        int result  = a+b ; 
+        System.out.println(result);
+    }
+    
+}
