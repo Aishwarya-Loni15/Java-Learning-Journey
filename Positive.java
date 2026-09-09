8e8ce03f46fffa7b895ebb58da822ee2e47326ff
Aishwarya-Loni15 <aishwaryaloni872@gmail.com>
2026-09-09 12:37:00 +0530

    Daily Java Learning

---
+++ Positive.java
@@ -0,0 +1,13 @@
+public class Positive{
+    public static void main(String args[]){
+        int n =-5;
+
+        if(n>0)
+            System.out.print("Positive");
+        else if (n<0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+        
+    }
+}
