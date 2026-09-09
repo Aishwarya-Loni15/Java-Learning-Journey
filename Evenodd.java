3a27f104a68cc11f450c81addc982240503e4840
Aishwarya-Loni15 <aishwaryaloni872@gmail.com>
2026-09-09 12:32:04 +0530

    Daily Java Learning

---
+++ Evenodd.java
@@ -0,0 +1,10 @@
+public class Evenodd {
+    public static void main(String args[]){
+        int n =10;
+        if (n%2==0)
+            System.out.print("Even");
+        else
+          System.out.println("odd");
+          
+    }
+}
