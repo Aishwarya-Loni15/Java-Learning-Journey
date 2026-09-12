e867d3e73c5984a3c8111b55204b304adc66841e
Aishwarya-Loni15 <aishwaryaloni872@gmail.com>
2026-09-12 21:59:11 +0530

    Daily Java Learning

---
+++ Overloading.java
@@ -0,0 +1,28 @@
+class Calculator {
+
+    // Method with 2 integer parameters
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method with 3 integer parameters
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method with 2 double parameters
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Overloading {
+    public static void main(String[] args) {
+
+        Calculator obj = new Calculator();
+
+        System.out.println(obj.add(10, 20));
+        System.out.println(obj.add(10, 20, 30));
+        System.out.println(obj.add(10.5, 20.5));
+    }
+}
