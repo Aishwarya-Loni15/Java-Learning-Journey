6be5275b78f63947d3420ffca5099fa5ef7ce050
Aishwarya-Loni15 <aishwaryaloni872@gmail.com>
2026-09-20 15:03:19 +0530

    Daily Java Learning

---
+++ Reverseword.java
@@ -0,0 +1,12 @@
+public class Reverseword {
+    public static void main(String[] args) {
+        String str = "Java is easy";
+        String[] words = str.split(" ");
+        for (String word : words) {
+            for (int i = word.length() - 1; i >= 0; i--) {
+                System.out.print(word.charAt(i));
+            }
+            System.out.print(" ");
+        }
+    }
+}
