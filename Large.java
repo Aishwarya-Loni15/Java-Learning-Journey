eaf8fdab4f468716606afd236ade80539069b768
Aishwarya-Loni15 <aishwaryaloni872@gmail.com>
2026-09-21 22:05:05 +0530

    Daily Java Learning

--- Large.java
+++ Large.java
@@ -1,4 +1,12 @@
+public class Large{
+    public static void main(String[]args){
+        int[] numbers={12,3,4,23,43,45};
+        int largest= numbers[0];
+        for(int i=1;i<numbers.length;i++){
+            if(numbers[i]>largest){
+                largest=numbers[i];
 
+            }
         }
         System.out.println("largest "+largest);  
     }
