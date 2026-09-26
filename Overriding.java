dd5d38c7c985e168d7626433ba7747547dce47ff
Aishwarya-Loni15 <aishwaryaloni872@gmail.com>
2026-09-26 10:50:34 +0530

    Daily Java Learning

--- Overriding.java
+++ Overriding.java
@@ -12,3 +12,14 @@ class Dog extends Animal {
     void sound() {
         System.out.println("Dog barks");
     }
+}
+
+public class Overriding {
+
+    public static void main(String[] args) {
+
+        Dog obj = new Dog();
+
+        obj.sound();
+    }
+}
